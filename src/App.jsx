0c4fa451da98005a42b0f00d81f9f6b9944b6137
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-04-13 20:11:49 +0700

    d

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <Navbar/>
       <Routes>
-        <Route path='/GrabFoodApp' element={<HomePage/>}/>
+        <Route path='/test-deploy' element={<HomePage/>}/>
       </Routes>
     </>
   )
