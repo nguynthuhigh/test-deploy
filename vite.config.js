47cdb8abbc8ce68f521dc47a7ffbdf845a6161fa
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-04-13 20:09:52 +0700

    d

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/GrabFoodApp", plugins: [react()], }) 
+export default defineConfig({ base: "/test-deploy", plugins: [react()], }) 
